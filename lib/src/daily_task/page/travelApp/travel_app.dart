766f5d2f4d7ed0d823450cef1789edbce3306302
santamayengb <19mca8232@cuchd.in>
2021-10-13 07:04:44 +0530

    DefaultTabController

--- lib/src/daily_task/page/travelApp/travel_app.dart
+++ lib/src/daily_task/page/travelApp/travel_app.dart
@@ -11,101 +11,94 @@ class _TravelAppState extends State<TravelApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(slivers: [
-        SliverAppBar(
-          flexibleSpace: FlexibleSpaceBar(
-            // title: Text('Where would you like to go?'),
-            background: Padding(
-              padding: const EdgeInsets.only(
-                left: 25,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Where would you like to go?',
-                    style: Theme.of(context)
-                        .textTheme
-                        .headline4
-                        ?.copyWith(color: Colors.white),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          pinned: true,
-          floating: false,
-          snap: false,
-          backgroundColor: Colors.orange,
-          leading: IconButton(
-              onPressed: () {
-                debugPrint('Menu button pressed');
-              },
-              icon: const Icon(Icons.menu)),
-          actions: const [
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: CircleAvatar(
-                backgroundColor: Colors.grey,
-                child: Icon(Icons.person),
-              ),
-            ),
-          ],
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(30),
-              bottomRight: Radius.circular(30),
-            ),
-          ),
-          expandedHeight: 250,
-          // bottom: const PreferredSize(
-          //   preferredSize: Size.fromHeight(200),
-          //   child: SizedBox(),
-          // ),
-        ),
-        SliverList(
-          delegate: SliverChildListDelegate([
-            DefaultTabController(
-              initialIndex: 1,
-              length: 3,
-              child: Container(
-                margin: const EdgeInsets.all(12),
-                child: Row(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            flexibleSpace: FlexibleSpaceBar(
+              // title: Text('Where would you like to go?'),
+              background: Padding(
+                padding: const EdgeInsets.only(
+                  left: 25,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    InkWell(
-                      onTap: () {
-                        debugPrint('Popular');
-                      },
-                      child: const Tab(
-                        text: 'Popular',
-                      ),
-                    ),
-                    const SizedBox(width: 20),
-                    InkWell(
-                      onTap: () {
-                        debugPrint('Recommended');
-                      },
-                      child: const Tab(
-                        text: 'Recommended',
-                      ),
-                    ),
-                    const SizedBox(width: 20),
-                    InkWell(
-                      onTap: () {
-                        debugPrint('Cost-effect');
-                      },
-                      child: const Tab(
-                        text: 'Cost-effect',
-                      ),
+                    Text(
+                      'Where would you like to go?',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline4
+                          ?.copyWith(color: Colors.white),
                     ),
                   ],
                 ),
               ),
             ),
-          ]),
-        )
-      ]),
+            pinned: true,
+            floating: false,
+            snap: false,
+            backgroundColor: Colors.orange,
+            leading: IconButton(
+                onPressed: () {
+                  debugPrint('Menu button pressed');
+                },
+                icon: const Icon(Icons.menu)),
+            actions: const [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  child: Icon(Icons.person),
+                ),
+              ),
+            ],
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30),
+              ),
+            ),
+            expandedHeight: 250,
+            // bottom: const PreferredSize(
+            //   preferredSize: Size.fromHeight(200),
+            //   child: SizedBox(),
+            // ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                DefaultTabController(
+                  initialIndex: 1,
+                  length: 3,
+                  child: Row(
+                    children: const [
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        child: Tab(
+                          text: 'Popular',
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        child: Tab(
+                          text: 'Recommended',
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        child: Tab(
+                          text: 'Cost-effect',
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
