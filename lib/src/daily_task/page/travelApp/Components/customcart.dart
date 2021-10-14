439f619bac52974dd126f9ce41697587115ca11a
santamayengb <19mca8232@cuchd.in>
2021-10-14 07:40:03 +0530

    image: AssetImage('assets/images/865178.jpg'),             fit: BoxFit.cover,

--- lib/src/daily_task/page/travelApp/Components/customcart.dart
+++ lib/src/daily_task/page/travelApp/Components/customcart.dart
@@ -11,7 +11,9 @@ class MyCustomCard extends StatelessWidget {
         decoration: BoxDecoration(
           color: const Color(0xff7c94b6),
           image: const DecorationImage(
-              image: AssetImage("assets/images/865178.jpg")),
+            image: AssetImage('assets/images/865178.jpg'),
+            fit: BoxFit.cover,
+          ),
           border: Border.all(
             color: Colors.black,
             width: 8,
