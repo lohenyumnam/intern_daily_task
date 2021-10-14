b7b882d3d6d5786360336ce7fda21f061c36615b
santamayengb <19mca8232@cuchd.in>
2021-10-14 07:33:23 +0530

    AssetImage

--- lib/src/daily_task/page/travelApp/Components/customcart.dart
+++ lib/src/daily_task/page/travelApp/Components/customcart.dart
@@ -7,23 +7,18 @@ class MyCustomCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(children: [
       Positioned(
-        child: Container(
-          margin: const EdgeInsets.all(10),
-          height: 500,
-          width: 350,
-          child: Image.asset('assets/images/865178.jpg', fit: BoxFit.cover),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: Colors.red,
-            border: const Border(
-              left: BorderSide(
-                color: Colors.green,
-                width: 10,
-              ),
-            ),
+          child: Container(
+        decoration: BoxDecoration(
+          color: const Color(0xff7c94b6),
+          image: const DecorationImage(
+              image: AssetImage("assets/images/865178.jpg")),
+          border: Border.all(
+            color: Colors.black,
+            width: 8,
           ),
+          borderRadius: BorderRadius.circular(12),
         ),
-      ),
+      )),
       Positioned(
         top: 15,
         right: 15,
