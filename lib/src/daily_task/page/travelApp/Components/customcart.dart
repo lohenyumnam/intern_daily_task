d705d019bf5c593a679fbdd6435354af148ff86f
santamayengb <19mca8232@cuchd.in>
2021-10-14 07:02:11 +0530

    Stack

--- lib/src/daily_task/page/travelApp/Components/customcart.dart
+++ lib/src/daily_task/page/travelApp/Components/customcart.dart
@@ -5,12 +5,21 @@ class MyCustomCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(10),
-      height: 500,
-      width: 350,
-      color: Colors.red,
-      child: Image.asset('assets/images/865178.jpg', fit: BoxFit.cover),
-    );
+    return Stack(children: [
+      Positioned(
+        child: Container(
+          margin: const EdgeInsets.all(10),
+          height: 500,
+          width: 350,
+          color: Colors.red,
+          child: Image.asset('assets/images/865178.jpg', fit: BoxFit.cover),
+        ),
+      ),
+      const Positioned(
+        top: 15,
+        right: 15,
+        child: IconButton(onPressed: null, icon: Icon(Icons.favorite)),
+      )
+    ]);
   }
 }
