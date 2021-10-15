eece7fb03de243e9058140f4df5fa9c6379c1516
santamayengb <19mca8232@cuchd.in>
2021-10-15 11:57:21 +0530

    working on tabbar

--- lib/src/daily_task/page/travelApp/Components/customtabs.dart
+++ lib/src/daily_task/page/travelApp/Components/customtabs.dart
@@ -3,38 +3,38 @@ import 'package:flutter/material.dart';
 class MyCustomTabs extends StatelessWidget {
   const MyCustomTabs({Key? key}) : super(key: key);
 
+  final double sBox = 50.0;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(20),
       child: Column(
         children: [
-          Row(
-            children: [
-              Expanded(
-                child: InkWell(
-                  onTap: () {
-                    debugPrint('Popular tapped.');
-                  },
-                  child: const Text('Popular'),
+          Stack(children: [
+            Row(
+              children: [
+                InkWell(
+                  onTap: () {},
+                  child: const Text("Popular"),
                 ),
-              ),
-              Expanded(
-                child: InkWell(
-                    onTap: () {
-                      debugPrint('Recomended tapped.');
-                    },
-                    child: const Text('Recomended')),
-              ),
-              Expanded(
-                child: InkWell(
-                    onTap: () {
-                      debugPrint('Cost-effect tapped.');
-                    },
-                    child: const Text('Cost-effect')),
-              ),
-            ],
-          ),
+                SizedBox(
+                  width: sBox,
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: const Text("Recommended"),
+                ),
+                SizedBox(
+                  width: sBox,
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: const Text("Cost-Effect"),
+                ),
+              ],
+            ),
+          ])
         ],
       ),
     );
