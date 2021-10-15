2da391ebce57fe23dd00546ee92207efe8ef4886
santamayengb <19mca8232@cuchd.in>
2021-10-15 14:35:14 +0530

    customtabs.dart error in line 35,

--- lib/src/daily_task/page/travelApp/Components/customtabs.dart
+++ lib/src/daily_task/page/travelApp/Components/customtabs.dart
@@ -1,42 +1,39 @@
 import 'package:flutter/material.dart';
 
-class MyCustomTabs extends StatelessWidget {
+class MyCustomTabs extends StatefulWidget {
   const MyCustomTabs({Key? key}) : super(key: key);
 
+  @override
+  State<MyCustomTabs> createState() => _MyCustomTabsState();
+}
+
+class _MyCustomTabsState extends State<MyCustomTabs> {
   final double sBox = 50.0;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20),
-      child: Column(
-        children: [
-          Stack(children: [
-            Row(
-              children: [
-                InkWell(
-                  onTap: () {},
-                  child: const Text("Popular"),
+    return Column(
+      children: const [
+        DefaultTabController(
+          length: 3,
+          initialIndex: 0,
+          child: TabBar(
+              labelColor: Colors.black,
+              indicatorColor: Colors.orange,
+              tabs: [
+                Tab(
+                  text: 'Popular',
                 ),
-                SizedBox(
-                  width: sBox,
+                Tab(
+                  text: 'Recommended',
                 ),
-                InkWell(
-                  onTap: () {},
-                  child: const Text("Recommended"),
-                ),
-                SizedBox(
-                  width: sBox,
-                ),
-                InkWell(
-                  onTap: () {},
-                  child: const Text("Cost-Effect"),
-                ),
-              ],
-            ),
-          ])
-        ],
-      ),
+                Tab(
+                  text: 'Cost-effect',
+                )
+              ]),
+        ),
+        TabBarView(controller: null, children: [Text("data")])
+      ],
     );
   }
 }
